a1112436a84b5dbdd61c4b878d9ac2023202ba27
kyanesdev <kyanesdev@gmail.com>
2021-10-05 23:29:49 -0300

    mas js

--- JS/nombre.js
+++ JS/nombre.js
@@ -1,1 +1,10 @@
-prompt("decime tu nombre");
+
+
+alert("Bienvenido a la pagina de kevin");
+
+cadena1 = "hola me llamo kevin, ¿";
+cadena2 = "como estas?";
+
+frase = cadena1 + cadena2;
+
+document.write(frase);
