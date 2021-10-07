41c8883fa23bb6b9d3fc6385600f83040c5dec30
kyanesdev <kyanesdev@gmail.com>
2021-10-07 01:32:02 -0300

    intento de ts fallido , asi que volvemos a usar js

---
+++ JS/nombre.js
@@ -0,0 +1,4 @@
+alert("Bienvenido a la pagina de kevin");
+var cadena1 = "hola como te va";
+prompt("decime tu nombre");
+document.write(cadena1);
--- TS/nombre.ts
+++
@@ -1,7 +0,0 @@
-
-
-alert("Bienvenido a la pagina de kevin");
-
-let cadena1="hola como te va";
-
-document.write(cadena1);
