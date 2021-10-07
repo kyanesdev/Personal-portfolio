b1731e1c4690db521008a9b39c74497b74175dd2
kyanesdev <kyanesdev@gmail.com>
2021-10-07 03:22:50 -0300

    sigo practicando

--- JS/nombre.js
+++ JS/nombre.js
@@ -1,4 +1,25 @@
-alert("Bienvenido a la pagina de kevin");
-var cadena1 = "hola como te va";
-prompt("decime tu nombre");
-document.write(cadena1);
+let pc = {
+
+    nombre: "KevinPC",
+    ram: "16gb",
+    procesador: "ryzen 5 5600x" 
+
+};
+
+document.write(pc["nombre"]);
+
+
+let num1 = prompt("Decime primer valor");
+let num2 = prompt("Decime segundo valor");
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+function suma(num1,num2){
+
+    let resultado = num1+num2;
+    return document.write(resultado);
+    
+}
+
+suma(num1,num2);
